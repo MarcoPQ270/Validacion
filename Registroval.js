e7885fa2ffd007695b4bbe346da22240110d7708
MarcoPQ <marco.balderaspq@gmail.com>
2019-06-11 18:53:37 -0500

    Se agrego un nuevo campo en el html y se agrego la validacion en el javascript

--- Registroval.js
+++ Registroval.js
@@ -10,6 +10,7 @@ function init(){
         correo:{required:true, email:true, minlength:8, maxlength:80},
         pwd:{required:true, maxlength:25},
         suel:{required:true, number:true},
+        edad:{required:true, number:true, maxlength:3},
         fec:{required:true, date:true}     
         }, 
         messages:
@@ -19,6 +20,7 @@ function init(){
         correo: {required:'El correo es requerido', email:'El formato de correo es incorrecto', minlength:'Escriba minimo 8 caracteres', maxlength:'Solo se permiten 80 caracteres'},
         pwd:{required:'La contraseña es requerida', minlength:'Se necesitan minimo 8 caracteres', maxlength:'Se necesitan maximo 25 caracteres'},
         suel:{required:'El sueldo es requerido', number:'Ingrese valores numericos'},
+        edad:{required:'la edad es requerida', number:'Ingrese valores numericos', maxlength:'Ingrese una edad real'},
         fec:{required:'La fecha es requerida'}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
         }, 
         
