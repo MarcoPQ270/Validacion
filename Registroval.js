326757e5e8416a9ae545ccf1a3d4b466c4789437
MarcoPQ <marco.balderaspq@gmail.com>
2019-06-11 21:05:43 -0500

    Se repararon los campos que no validaban

--- Registroval.js
+++ Registroval.js
@@ -6,23 +6,21 @@ function init(){
         rules:
         {
         nombre:{required:true},
-        apellidos:{required:true},
-        correo:{required:true, email:true, minlength:8, maxlength:80},
+        apellido:{required:true},
+        corr:{required:true, email:true, minlength:8, maxlength:80},
         pwd:{required:true, maxlength:25},
         suel:{required:true, number:true},
         edad:{required:true, number:true, maxlength:3},
-        fec:{required:true, date:true}     
-        }, 
+         }, 
         messages:
         {
         nombre:{required:'El nombre es requerido'},
-        apellidos:{required:'El apellido es requerido'},
-        correo: {required:'El correo es requerido', email:'El formato de correo es incorrecto', minlength:'Escriba minimo 8 caracteres', maxlength:'Solo se permiten 80 caracteres'},
+        apellido:{required:'El apellido es requerido'},
+        corr: {required:'El correo es requerido', email:'El formato de correo es incorrecto', minlength:'Escriba minimo 8 caracteres', maxlength:'Solo se permiten 80 caracteres'},
         pwd:{required:'La contraseña es requerida', minlength:'Se necesitan minimo 8 caracteres', maxlength:'Se necesitan maximo 25 caracteres'},
         suel:{required:'El sueldo es requerido', number:'Ingrese valores numericos'},
         edad:{required:'la edad es requerida', number:'Ingrese valores numericos', maxlength:'Ingrese una edad real'},
-        fec:{required:'La fecha es requerida'}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-        }, 
+         }, 
         
            errorElement:"div",
            errorClass:"invalid",
@@ -38,6 +36,6 @@ function init(){
    });
 }
 function saveData(){
-
-    alert("El formulario es valido y guardado");
+    alert("Formulario Enviado");
+    M.toast({html: 'Formuario Enviado', classes: 'rounded', displayLength: 4000});
 }
